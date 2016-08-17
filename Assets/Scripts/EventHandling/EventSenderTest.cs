5eefba61c548986a141005ab0517d1946606fb8b
Eduardo <cardenas.eduardo10@yahoo.com.au>
2016-08-17 22:09:21 +0200

    Random Spawner + Sheep Stop Less

---
+++ Assets/Scripts/EventHandling/EventSenderTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventSenderTest : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.K))
+	    {
+	        EventRelay.RelayEvent(EventRelay.EventMessageType.WolfHowled, this);
+	    }
+	}
+
+}

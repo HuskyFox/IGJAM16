dc3ba19f567e719eb20ee78451501f5d257f0070
Oscar <chiyuiri@gmail.com>
2016-08-19 21:17:39 +0930

    fghfhgh

--- Assets/Scripts/RestartGameButton.cs
+++ Assets/Scripts/RestartGameButton.cs
@@ -14,6 +14,15 @@ public class RestartGameButton : MonoBehaviour {
 	}
 
 	public void RestartGameScene() {
-		Application.LoadLevel(Application.loadedLevel);
+        PlayerManager playerManager = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
+
+        for (int i=1;i<=playerManager.players.Count;i++)
+        {
+            GameObject.Find("Player_" + i).SendMessage("RespawnPlayer");
+
+        }
+        SoundManager.instance.StopRestartMusic();
+        SoundManager.instance.PlayGameMusic();
+        Application.LoadLevel(Application.loadedLevel);
 	}
 }

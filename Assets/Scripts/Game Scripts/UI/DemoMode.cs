2ae58795a99e5560dcbd85e05bf9b6b15f723361
Jonas Dutouquet <jonas.dutouquet@gmail.com>
2017-11-21 12:46:56 +0100

    Added an instructions text to display for demos

---
+++ Assets/Scripts/Game Scripts/UI/DemoMode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class DemoMode : MonoBehaviour
+{
+	[SerializeField] private GameObject _instructions;
+
+	void Update () 
+	{
+		if(InputManager.ActiveDevice.LeftBumper.IsPressed)
+		{
+			if (InputManager.ActiveDevice.RightBumper.WasPressed)
+				_instructions.SetActive (!_instructions.activeSelf);
+		}
+
+		if(Input.GetKey (KeyCode.A))
+		{
+			if (Input.GetKeyDown (KeyCode.Z))
+				_instructions.SetActive (!_instructions.activeSelf);
+		}
+	}
+}
